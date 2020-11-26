9c4cd43c9c7ada8ad105692f7c1da34c97561751
ylim31 <ylim31@illinois.edu>
2020-11-25 21:08:35 -0600

    start filling graph

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <fstream>
 #include "cs225/PNG.h"
-int main(){
-    cs225::PNG png;
+#include "graph.h"
 
-    std::cout << "Hello, world!\n";
+using namespace std;
+int main(){
+    Graph g_(true, true);
+
+    ifstream lastfm_file;
+    lastfm_file.open("lastfm_node.csv");
+    ifstream lastfm_file;
+    lastfm_file.open("lastfm_neighbor.csv");
+
+    while (lastfm_file.good()) {
+        string node_label;
+        getline(lastfm_file, node_label, ',');
+        //std::cout << "NODEs: " << node_label;
+        g_.insertVertex(node_label);
+        
+        while (lastfm_file.good()) {
+            string ss;
+            string node_neighbor;
+            getline(lastfm_file, node_neighbor, ']');
+            stringstream s(node_neighbor);
+            while (getline(s, ss, ',')) {
+            
+            }
+        }
+
+    }
+    lastfm_file.close();
+
+    
+    
 }
