4dd2dc7b2f565714813a7c05eaf84b83dfbac138
ylim31 <ylim31@illinois.edu>
2020-12-03 11:10:43 -0600

    link test for  makefile and add test

--- tests/part1.cpp
+++ tests/part1.cpp
@@ -3,9 +3,22 @@
 
 #include "../cs225/PNG.h"
 #include "../cs225/HSLAPixel.h"
+#include "../dijkstra.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
 
 
 TEST_CASE("example", "[weight=1]") {
-  int i = 1;
-  REQUIRE( i == 1 );
+  
+  Dijkstra test("lastfm_node.csv", "lastfm_neighbor.csv");
+  Vertex start = to_string(0);
+  Vertex end = to_string(4557);
+  vector<Vertex> result = test.find_shortest_path(start, end);
+  for (unsigned i = 0; i < result.size(); i++) {
+    cout << result[i] << endl;
+  }
+  vector<Vertex> expected;
+  REQUIRE(result == expected);
 }
