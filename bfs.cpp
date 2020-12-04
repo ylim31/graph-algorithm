5c051b89324592f147b7275846f3b621f1c43f41
Nishita Amberkar <nda6@illinois.edu>
2020-12-04 10:52:39 -0600

    Updates

--- bfs.cpp
+++ bfs.cpp
@@ -2,6 +2,7 @@
 #include "graph.h"
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 #include <string>
 #include <string.h>
 #include <iostream>
@@ -9,23 +10,73 @@
 
 using namespace std;
 
-const int BFS::INF = 9999;
-const int BFS::EMPTY_VERTEX = -1;
-
 BFS::BFS() : g_(true, false) {}
 
 BFS::BFS(string node_filename, string neighbor_filename) : g_(true, false) {
-
 ifstream lastfm_file;
 ifstream lastfm_neighbor_file;
 lastfm_file.open(node_filename);
 lastfm_neighbor_file.open(neighbor_filename);
 
-lastfm_neighbor_file.close();
+while (lastfm_file.good()) {
+    string node_label;
+    getline(lastfm_file, node_label, ',');
+    g_.insertVertex(node_label); 
+}
 
+lastfm_file.close();
+
+size_t i = 0;
+while (lastfm_neighbor_file.good()) {
+        string each;
+        string node_neighbor;
+        getline(lastfm_neighbor_file, node_neighbor, ']');
+        char node_neighbor_char[node_neighbor.size()];
+        strcpy(node_neighbor_char, node_neighbor.c_str());
+        
+        char* piece = strtok(node_neighbor_char, " , \" \t []");
+
+        while (piece != NULL) { 
+
+            g_.insertEdge(to_string(i), piece);
+            piece = strtok(NULL, ", \" []");      
+        }
+        i++;
+    }
+
+lastfm_neighbor_file.close();
 }
 
 void BFS::traversal(Vertex start) {
-    
+    unordered_map<Vertex, bool> is_visited_;
+
+    /**
+    bool *visited = new bool[V]; 
+    for(int i = 0; i < V; i++) 
+        visited[i] = false; 
+  
+    list<int> queue; 
+  
+    visited[s] = true; 
+    queue.push_back(s); 
+
+    list<int>::iterator i; 
+  
+    while(!queue.empty()) 
+    {  
+        s = queue.front(); 
+        cout << s << " "; 
+        queue.pop_front(); 
+  
+        for (i = adj[s].begin(); i != adj[s].end(); ++i) 
+        { 
+            if (!visited[*i]) 
+            { 
+                visited[*i] = true; 
+                queue.push_back(*i); 
+            } 
+        } 
+    } **/
+
 }
 
--- bfs.h
+++ bfs.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <unordered_map>
 
 class BFS {
     public:
@@ -12,10 +13,6 @@ class BFS {
         BFS(string node_filename, string neighbor_filename);
         void traversal(Vertex start);
     private:
-        const static int INF;
-        const static int EMPTY_VERTEX;
-
         int numVertices_;
-        vector<Vertex> adj_;
         Graph g_;
 };
