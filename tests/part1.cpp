355b6f8a6bb5bd66bd716aabd2dff1c939ac0017
jaeminushyuklee <jaeminushyuklee@gmail.com>
2020-12-08 23:47:28 +0900

    added more dijkstra baby tests

--- tests/part1.cpp
+++ tests/part1.cpp
@@ -8,15 +8,31 @@
 #include <vector>
 #include <iostream>
 
+using namespace std;
+
+#include <iostream>
 #include <string>
 
 using namespace std;
 
-
-TEST_CASE("example", "[weight=1]") {
-  
-  Dijkstra test("lastfm_node.csv", "lastfm_neighbor.csv");
+TEST_CASE("baby find shortest path 1", "[weight=1]") {
+  Dijkstra test("d_baby_test_node.csv", "d_baby_test_neighbor.csv");
   Vertex start = to_string(0);
+  Vertex end = to_string(2);
+  vector<Vertex> result = test.find_shortest_path(start, end);
+  for (unsigned i = 0; i < result.size(); i++) {
+    cout << result[i] << endl;
+  }
+  vector<Vertex> expected;
+  expected.push_back(Vertex("0"));
+  expected.push_back(Vertex("1"));
+  expected.push_back(Vertex("2"));
+  REQUIRE(result == expected);
+}
+
+TEST_CASE("baby find shortest path on itself", "[weight=1]") {
+  Dijkstra test("d_baby_test_node.csv", "d_baby_test_neighbor.csv");
+  Vertex start = to_string(2);
   Vertex end = to_string(2);
   vector<Vertex> result = test.find_shortest_path(start, end);
   for (unsigned i = 0; i < result.size(); i++) {
@@ -26,11 +42,27 @@ TEST_CASE("example", "[weight=1]") {
   REQUIRE(result == expected);
 }
 
-TEST_CASE("2", "[weight=1]") {
+TEST_CASE("baby find shortest path 2", "[weight=1]") {
+  Dijkstra test("d_baby_test_node.csv", "d_baby_test_neighbor.csv");
+  Vertex start = to_string(3);
+  Vertex end = to_string(2);
+  vector<Vertex> result = test.find_shortest_path(start, end);
+  for (unsigned i = 0; i < result.size(); i++) {
+    cout << result[i] << endl;
+  }
+  vector<Vertex> expected;
+  expected.push_back(Vertex("3"));
+  expected.push_back(Vertex("1"));
+  expected.push_back(Vertex("2"));
+  REQUIRE(result == expected);
+}
+
+
+TEST_CASE("example", "[weight=1]") {
   
-  Dijkstra test("different_component.csv", "different_component_neighbor.csv");
+  Dijkstra test("lastfm_node.csv", "lastfm_neighbor.csv");
   Vertex start = to_string(0);
-  Vertex end = to_string(4);
+  Vertex end = to_string(4557);
   vector<Vertex> result = test.find_shortest_path(start, end);
   for (unsigned i = 0; i < result.size(); i++) {
     cout << result[i] << endl;
