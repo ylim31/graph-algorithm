12b4e85a43e2ce37d604c060b8146c140edba043
Nishita Amberkar <nda6@illinois.edu>
2020-12-09 18:02:26 -0600

    Components Tests

--- tests/bfs_test.cpp
+++ tests/bfs_test.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-TEST_CASE(" Test 1 ", "[weight=1]") {
+TEST_CASE(" Test Simple Graph - Beginning", "[weight=1]") {
   BFS test("BFS-csv/bfs_baby_test_neighbor.csv", "BFS-csv/bfs_baby_test_neighbor.csv");
   Vertex start = to_string(2);
   vector<Vertex> result = test.traversal(start);
@@ -19,7 +19,7 @@ TEST_CASE(" Test 1 ", "[weight=1]") {
   REQUIRE(result[0] == "2");
 }
 
-TEST_CASE(" Test 2 ", "[weight=1]") {
+TEST_CASE(" Test Graph with Cycle - End", "[weight=1]") {
   BFS test("BFS-csv/bfs_baby_test_neighbor.csv", "BFS-csv/bfs_baby_test_neighbor.csv");
   Vertex start = to_string(2);
   vector<Vertex> result = test.traversal(start);
@@ -28,7 +28,7 @@ TEST_CASE(" Test 2 ", "[weight=1]") {
   REQUIRE(result[length - 1] == "0");
 }
 
-TEST_CASE(" Test 3 ", "[weight=1]") {
+TEST_CASE(" Test Graph with Cycle - Traversal ", "[weight=1]") {
   BFS test("BFS-csv/bfs_baby_test_neighbor.csv", "BFS-csv/bfs_baby_test_neighbor.csv");
   Vertex start = to_string(2);
   vector<Vertex> result = test.traversal(start);
@@ -38,11 +38,12 @@ TEST_CASE(" Test 3 ", "[weight=1]") {
   expected.push_back(Vertex("3"));
   expected.push_back(Vertex("1"));
   expected.push_back(Vertex("0"));
-    
+  
+  REQUIRE(result.size() == expected.size());
   REQUIRE(result == expected);
 }
 
-TEST_CASE(" Test 4 ", "[weight=2]") {
+TEST_CASE(" Test Graph - Traversal ", "[weight=2]") {
   BFS test("BFS-csv/bfs_baby_test_neighbor 2.csv", "BFS-csv/bfs_baby_test_neighbor 2.csv");
   Vertex start = to_string(0);
   vector<Vertex> result = test.traversal(start);
@@ -53,8 +54,22 @@ TEST_CASE(" Test 4 ", "[weight=2]") {
   expected.push_back(Vertex("2"));
   expected.push_back(Vertex("1"));
   expected.push_back(Vertex("4"));
-    
+  
+  REQUIRE(result.size() == expected.size());
   REQUIRE(result == expected);
 } 
 
-
+TEST_CASE(" Test Different Components Graph - Traversal ", "[weight=2]") {
+  BFS test("BFS-csv/different_component_neighbor.csv", "BFS-csv/different_component.csv");
+  Vertex start = to_string(1);
+  vector<Vertex> result = test.traversal(start);
+  
+  vector<Vertex> expected;
+  expected.push_back(Vertex("1"));
+  expected.push_back(Vertex("2"));
+  expected.push_back(Vertex("0"));
+  expected.push_back(Vertex("4"));
+  
+ // REQUIRE(result.size() == expected.size());
+  REQUIRE(result == expected);
+} 
