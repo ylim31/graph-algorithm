99431e617ac3392a4c0a3fff7e41567566b06ba2
ylim31 <ylim31@illinois.edu>
2020-12-09 23:44:40 -0600

    just commiting so i can pull

--- landmark.cpp
+++ landmark.cpp
@@ -19,8 +19,9 @@ vector<Vertex> LandMark::find_shortest_path(Vertex start, Vertex end, vector<Ver
     landmarks.insert(landmarks.begin(), start);
     landmarks.push_back(end);
 
+
     for (unsigned i = 0; i < landmarks.size() - 1; i++) {
-        shortest_path.insert(shortest_path.begin(), shortest_path.end(), d_.find_shortest_path(landmarks[i], landmarks[i+1]).end());
+        shortest_path.insert(shortest_path.end(), shortest_path.begin(), d_.find_shortest_path(landmarks[i], landmarks[i+1]).end());
     }
     return shortest_path;
     
